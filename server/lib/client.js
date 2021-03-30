af0e1f4d68460e23ec60a5b205e85e2c96462408
afreidz <afreidenfelds@gmail.com>
2021-03-30 15:06:43 -0500

    adds socket client class for handling messaging rather than a worker thread

---
+++ server/lib/client.js
@@ -0,0 +1,120 @@
+const { OPEN } = require('ws');
+const status = require('../status');
+const { spawn } = require('child_process');
+const logger = require('../../lib/logger');
+const config = require('../../config.proxy');
+const clients = new Set();
+
+const binding = spawn(`sh`, ['-c', `i3-msg -m -t subscribe '["binding"]'`]);
+const workspace = spawn(`sh`, ['-c', `i3-msg -m -t subscribe '["workspace"]'`]);
+
+class ListedSet extends Set {
+	constructor(args) {
+		super(args);
+	}
+	get list() { return [...this] }
+}
+
+class Client {
+	#ws = null;
+	#intervals = [];
+
+	constructor(ws, scopes = []) {
+		this.#ws = ws;
+		this.active = true;
+		this.scopes = new ListedSet();
+
+		scopes.forEach(s => this.scopes.add(s));
+
+		ws.on('close', () => {
+			clients.delete(this);
+			logger.log(`socket disconnected | scopes: [${this.scopes.list.join(',')}]`);
+		});
+
+		clients.add(this);
+		if (this.ready && !this.ipc) this.update();
+		logger.log(`socket connected | scopes: [${this.scopes.list.join(',')}]`);
+	}
+
+	get ipc() {
+		return this.#ws.protocol === 'ipc';
+	}
+
+	get ready() {
+		return this.#ws.readyState === OPEN;
+	}
+
+	send(data) {
+		this.#ws.send(JSON.stringify(data));
+		return this;
+	}
+
+	rawsend(data) {
+		this.#ws.send(data);
+		return this;
+	}
+
+	async update() {
+		if (this.ipc) return this;
+		if (!this.active) return this;
+
+		if (this.scopes.has('dash')) {
+			status.me().then(d => this.send(d));
+			status.music().then(d => this.send(d));
+			status.volume().then(d => this.send(d));
+			status.battery().then(d => this.send(d));
+			status.network().then(d => this.send(d));
+			status.displays().then(d => this.send(d));
+			status.bluetooth().then(d => this.send(d));
+			status.brightness().then(d => this.send(d));
+			if (config.secretstuff) status.devices().then(d => this.send(d));
+		}
+
+		if (this.scopes.has('launch')) {
+			status.apps().then(d => this.send(d));
+		}
+
+		if (this.scopes.has('bar')) {
+			status.workspaces().then(d => this.send(d));
+		}
+		return this;
+	}
+
+	start() {
+		if (this.ipc) return this;
+		if (this.#intervals.length) this.#intervals.forEach(clearInterval);
+
+		if (this.scopes.has('dash')) {
+			this.#intervals.push(setInterval(() => status.perf().then(d => this.send(d)), 5000));
+
+			this.#intervals.push(setInterval(() => {
+				status.music().then(d => this.send(d));
+				if (config.secretstuff) status.devices().then(d => this.send(d));
+			}, 1000));
+
+			this.#intervals.push(setInterval(() => {
+				status.battery().then(d => this.send(d));
+				status.network().then(d => this.send(d));
+				status.bluetooth().then(d => this.send(d));
+			}, 5000));
+		}
+
+		if (this.scopes.has('bar')) {
+			binding.stdout.on('data', d => status.layout(d).then(d => this.send(d)));
+			workspace.stdout.on('data', () => status.workspaces().then(d => this.send(d)));
+		}
+
+		if (this.scopes.has('launch')) {
+			this.#intervals.push(setInterval(() => status.apps().then(d => this.send(d)), 5000));
+		}
+		return this;
+	}
+
+	static byScope(s) {
+		return [...clients].filter(c => {
+			return c.scopes.list.includes(s) && c.ready;
+		});
+	}
+}
+
+module.exports = Client;
--- server/socketsetup.js
+++ server/socketsetup.js
@@ -1,32 +1,8 @@
 const url = require('url');
-const { OPEN } = require('ws');
 const status = require('./status');
-const worker = require('./lib/worker');
+const Client = require('./lib/client');
 const logger = require('../lib/logger');
 const { ips, views } = require('../consts');
-const clients = new Set();
-
-worker.on('dash', data => {
-	const dashclients = [...clients].filter(c => (c.scopes.includes('dash') && c.ws.protocol !== 'ipc'));
-	if (dashclients.length === 0) return;
-	if (data.id === 'perf') return dashclients.forEach(c => c.send(data));
-
-	dashclients.forEach(c => {
-		if (c.active) c.send(data);
-	});
-});
-
-worker.on('bar', data => {
-	const barclients = [...clients].filter(c => (c.scopes.includes('bar') && c.ws.protocol !== 'ipc'));
-	if (barclients.length === 0) return;
-	barclients.forEach(c => c.send(data));
-});
-
-worker.on('launch', data => {
-	const launchclients = [...clients].filter(c => (c.scopes.includes('launch') && c.ws.protocol !== 'ipc'));
-	if (launchclients.length === 0) return;
-	launchclients.forEach(c => c.send(data));
-});
 
 module.exports = function (wss, http) {
 	http.on('upgrade', (req, socket, head) => {
@@ -45,50 +21,14 @@ module.exports = function (wss, http) {
 			return ws.terminate();
 		}
 
-		const client = { 
-			ws,
-			scopes: [],
-			active: true,
-			send(data) {
-				ws.send(JSON.stringify(data));
-			} 
-		};
-		
-		if (pathname === 'all') {
-			client.scopes = [...views, 'app'];
-		} else if (pathname !== '') {
-			client.scopes = [pathname];
-		}
+		const scopes = pathname === 'all'
+			? [...views, 'app']
+			: pathname !== ''
+				? [pathname]
+				: [];
 
-		clients.add(client);
-
-		if (client.scopes.includes('dash')) {
-			client.send(await status.me());
-			client.send(await status.music());
-			client.send(await status.volume());
-			client.send(await status.battery());
-			client.send(await status.devices());
-			client.send(await status.network());
-			client.send(await status.displays());
-			client.send(await status.bluetooth());
-			client.send(await status.brightness());
-		}
-		
-		if (client.scopes.includes('launch')) {
-			client.send(await status.apps());
-		}
-
-		if (client.scopes.includes('bar')) {
-			client.send(await status.workspaces());
-		}
-		
-		ws.on('close', () => {
-			logger.log(`socket disconnected | scopes: [${client.scopes.join(',')}]`);
-		});
-
+		const client = new Client(ws, scopes).start();
 		ws.on('message', msg => handleMsg(msg, client));
-		
-		logger.log(`socket connected | scopes: [${client.scopes.join(',')}]`);
 	});
 }
 
@@ -100,10 +40,7 @@ async function handleMsg(msg, client) {
 		if (data.action === 'idle') client.active = false;
 		if (data.action === 'active') client.active = true;
 	} else {
-		clients.forEach(c => {
-			if (c.scopes.includes(data.target) && c.active && c.ws.readyState === OPEN) {
-				c.ws.send(msg);
-			}
-		});
+		const clients = Client.byScope(data.target);
+		clients.forEach(c => c.rawsend(msg));
 	}
 }
