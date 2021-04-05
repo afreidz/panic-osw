7e50cf015d656513ebc19cfd24afafb9058e506a
afreidz <afreidenfelds@gmail.com>
2021-04-05 09:49:53 -0500

    lint

--- app/components/lib/inside.js
+++ app/components/lib/inside.js
@@ -4,8 +4,8 @@ export default function inside(target, radius, point) {
 	const { x, y } = point;
 
 	let r = radius;
-  const dist = (a - x) * (a - x) + (b - y) * (b - y);
-  r *= r;
+	const dist = (a - x) * (a - x) + (b - y) * (b - y);
+	r *= r;
 
-  return dist < r;
+	return dist < r;
 }
